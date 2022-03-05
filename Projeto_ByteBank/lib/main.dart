6651f0fbc078f47b6661c24231836a54b5182dad
Bruno Cheles <bruno.cheles@outlook.com>
2022-03-04 22:16:14 -0300

    Primeira parte do app no Flutter
    Criação do layout. 25% do curso feito.

--- Projeto_ByteBank/lib/main.dart
+++ Projeto_ByteBank/lib/main.dart
@@ -1,5 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Text('Bem vindo ao ByteBank!', textDirection: TextDirection.ltr,));
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('100.00'),
+              subtitle: Text('3247-0'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('200.00'),
+              subtitle: Text('3247-0'),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    ),
+  ));
 }
