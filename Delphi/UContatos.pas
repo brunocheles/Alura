a8e1ac9e45c435aa58f9565e6049695a44a7158b
Bruno Cheles <53266946+brunocheles@users.noreply.github.com>
2022-03-07 14:34:28 -0300

    Fim projeto1
    finalização do primeiro projeto em delphi.

--- Delphi/UContatos.pas
+++ Delphi/UContatos.pas
@@ -4,10 +4,42 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, Data.Win.ADODB;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, Data.Win.ADODB,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf,
+  FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async,
+  FireDAC.Phys, FireDAC.Phys.MSAcc, FireDAC.Phys.MSAccDef, FireDAC.VCLUI.Wait,
+  FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt,
+  System.Rtti, System.Bindings.Outputs, Vcl.Bind.Editors, Data.Bind.EngExt,
+  Vcl.Bind.DBEngExt, Data.Bind.Components, Vcl.StdCtrls, FireDAC.Comp.DataSet,
+  FireDAC.Comp.Client, Data.Bind.DBScope, Vcl.ExtCtrls, Vcl.DBCtrls, Vcl.Grids,
+  Vcl.DBGrids;
 
 type
   TForm_Contatos = class(TForm)
+    BDConexão: TFDConnection;
+    BindSourcecontatos: TBindSourceDB;
+    FDTablecontatos: TFDTable;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    BindingsList1: TBindingsList;
+    LinkControlToField1: TLinkControlToField;
+    LinkControlToField2: TLinkControlToField;
+    LinkControlToField3: TLinkControlToField;
+    LinkControlToField4: TLinkControlToField;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    DBNavigator1: TDBNavigator;
+    DataSource1: TDataSource;
+    DBGrid1: TDBGrid;
+    Memo1: TMemo;
+    LinkControlToField5: TLinkControlToField;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure DBGrid1DblClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +52,15 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm_Contatos.Button1Click(Sender: TObject);
+begin
+  Form_Contatos.Close;
+end;
+
+procedure TForm_Contatos.DBGrid1DblClick(Sender: TObject);
+begin
+showmessage ('Alteracoes somente pelos campos ' );
+end;
 
 end.
