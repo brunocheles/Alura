c20361d59ca9cc814f864a82c53636a648ffcf5d
Bruno Cheles <bruno.cheles@outlook.com>
2022-03-14 19:46:35 -0300

    Atualização simples

--- Javascript/index.js
+++ Javascript/index.js
@@ -15,16 +15,18 @@ const contaCorrenteCliente2 = new ContaCorrente();
 contaCorrenteCliente2.agencia = 3004;
 contaCorrenteCliente2.cliente = cliente2;
 
+
 console.log(contaCorrenteCliente1);
-console.log(contaCorrenteCliente2);
+console.log(contaCorrenteCliente2); 
 
 console.log(cliente1.nome, contaCorrenteCliente1.mostraSaldo);
 console.log(cliente2.nome, contaCorrenteCliente2.mostraSaldo);
 console.log(contaCorrenteCliente1.mostraAgencia);
-cliente1.colNome = "Bruno Cheles";
-console.log(cliente1.nome);
+
+cliente1.mudaNome = "Bruno Cheles";
+console.log(cliente1.nome, contaCorrenteCliente1.mostraSaldo);
 
 contaCorrenteCliente2.depositar(200);
 console.log(cliente2.nome, contaCorrenteCliente2.mostraSaldo);
 contaCorrenteCliente2.sacar(50);
-console.log(cliente2.nome, contaCorrenteCliente2.mostraSaldo);
+console.log(cliente2.nome, contaCorrenteCliente2.mostraSaldo); 
