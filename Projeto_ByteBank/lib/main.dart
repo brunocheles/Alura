e4a7c1eda82629166deace4e786b3742ff6a7565
Bruno Cheles <bruno.cheles@outlook.com>
2022-03-14 13:32:50 -0300

    Continuação curso Flutter

--- Projeto_ByteBank/lib/main.dart
+++ Projeto_ByteBank/lib/main.dart
@@ -6,28 +6,49 @@ void main() {
       appBar: AppBar(
         title: Text('Transferências'),
       ),
-      body: Column(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100.00'),
-              subtitle: Text('3247-0'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('200.00'),
-              subtitle: Text('3247-0'),
-            ),
-          ),
-        ],
-      ),
+      body: ListaTransferencias(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {},
       ),
     ),
   ));
+}
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(200.0,'3247-0')),
+        ItemTransferencia(Transferencia(300.0,'3247-1')),
+        ItemTransferencia(Transferencia(400.0,'3248-0')),
+        ItemTransferencia(Transferencia(100.0,'3248-1')),
+      ],
+    );
+  }
+}
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta),
+      ),
+    );
+  }
+}
+class Transferencia {
+  final double valor;
+  final String numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
