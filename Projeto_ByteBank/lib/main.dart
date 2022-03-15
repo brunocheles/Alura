88da6af89d5355e4e7dce66b4dd3a655428dc589
Bruno Cheles <bruno.cheles@outlook.com>
2022-03-14 22:23:32 -0300

    Criação da tela de inserção do usuário

--- Projeto_ByteBank/lib/main.dart
+++ Projeto_ByteBank/lib/main.dart
@@ -1,35 +1,97 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Transferências'),
-      ),
-      body: ListaTransferencias(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
-      ),
-    ),
-  ));
+  runApp(BytebankApp());
 }
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criando Transferência'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controladorCampoNumeroConta,
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                  labelText: 'Número da Conta', hintText: '0000'),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controladorCampoValor,
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Valor',
+                  hintText: '0.00'),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final String numeroConta = _controladorCampoNumeroConta.text;
+              final double valor = double.tryParse(_controladorCampoValor.text);
+              Transferencia(valor, numeroConta);
+            },
+            child: Text('Confirmar'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(200.0,'3247-0')),
-        ItemTransferencia(Transferencia(300.0,'3247-1')),
-        ItemTransferencia(Transferencia(400.0,'3248-0')),
-        ItemTransferencia(Transferencia(100.0,'3248-1')),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(200.0, '3247-0')),
+          ItemTransferencia(Transferencia(300.0, '3247-1')),
+          ItemTransferencia(Transferencia(400.0, '3248-0')),
+          ItemTransferencia(Transferencia(100.0, '3248-1')),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
-class ItemTransferencia extends StatelessWidget {
 
+class ItemTransferencia extends StatelessWidget {
   final Transferencia _transferencia;
 
   ItemTransferencia(this._transferencia);
@@ -46,6 +108,7 @@ class ItemTransferencia extends StatelessWidget {
     );
   }
 }
+
 class Transferencia {
   final double valor;
   final String numeroConta;
